7ec5039328e9e5c88aad4ae41f049441ce79af14
Alex Brown <alex@alexanderdbrown.com>
2014-03-10 13:03:18 +0000

    Made some changes to the daemon.

--- syslog_parse/daemon.py
+++ syslog_parse/daemon.py
@@ -1,10 +1,48 @@
 import json
+from threading import Thread
 from flask import Flask, request
 
-from syslog_parse.parse import SyslogParser
+from syslog_parse.watcher import LogWatcher
 
+class Daemon(Thread):
+  def __init__(self):
+    super(Daemon, self).__init__()
+    self.logs = []
+    self.processes = []
+
+  def start(self):
+    self.running = True
+    super(Daemon, self).start()
+
+  def run(self):
+    while self.running:
+      for watcher in self.logs:
+        try:
+          watcher.do_parse(self.do_statsd)
+        except Exception:
+          print "error"
+
+  def do_statsd(self, process, sev):
+    print "{0} - {1}".format(process, sev)
+
+  def stop(self):
+    print "Stopping..."
+    self.running = False
+
+  def add_log(self, log):
+    self.logs.append(LogWatcher(log, self.processes))
+
+  def del_log(self, log):
+    self.logs.remove(log)
+
+  def add_ps(self, ps):
+    self.logs.append(ps)
+
+  def del_ps(self, ps):
+    self.processes.remove(log)
 
 app = Flask(__name__)
+daemon = Daemon()
   
 @app.route("/", methods=["POST"])
 def post_run():
@@ -13,3 +51,27 @@ def post_run():
     return SyslogParser(logfile).parse()
   except IOError as e:
     return (json.dumps({"fileerror": str(e)}), 400)
+
+@app.route("/start", methods=["POST"])
+def start_daemon():
+  daemon.start()
+  return ("SysWatch started", 200)
+
+@app.route("/stop", methods=["POST"])
+def stop_daemon():
+  daemon.stop()
+  return ("SysWatch stopped", 200)
+
+@app.route("/log", methods=["POST"])
+def add_log():
+  logfile = request.values["logfile"]
+  daemon.add_log(logfile)
+  return ("added", 200)
+
+@app.route("/log/<id>", methods=["DELETE"])
+def del_log():
+  if "logfile" not in requests.values:
+    return (json.dumps({"error": "No logfile specified"}), 400)
+  logfile = request.values["logfile"]
+  daemon.del_log(logfile)
+  return ("", 200)
---
+++ syslog_parse/watcher.py
@@ -0,0 +1,31 @@
+import re
+
+class LogWatcher(object):
+  def __init__(self, log, processes):
+    print "Watching log {}".format(log)
+    self.log = log
+    self.pos = 0
+    self.processes = processes
+
+  def do_parse(self, statsd_callback):
+    """Analyses a logfile and counts the number of each severity exists.
+       This information is then put into JSON format."""
+    # format: <sf> timestamp machine prog: msg
+    with open(self.log) as file:
+      file.seek(self.pos)
+      for line in file:
+        print line
+        # regex to match the standard syslog format. Will group the priority.
+        matches = re.match(r"\<(\d\d?\d?)\>\s*...\s+\d\d?\s+\d\d:\d\d:\d\d\s+.+\s+.+:\s+.+", line)
+        if(matches == None):
+          import sys
+          sys.stderr.write("Not a syslog format\n")
+        else:
+          # Priority = facility * 8 + severity
+          pri = int(matches.group(1))
+          sev = pri % 8
+          print "{} in {}".format(sev, line)
+      self.pos = file.tell()
+
+  def set_processes(self, processes):
+    self.processes = processes
